c14b7da4ff5a94dbe871ae7830786fd61347f6f7
Prohor <prohor-yakovlev@mail.ru>
2022-03-08 12:32:32 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,53 @@
+"use strict";
+
+// const result = confirm("Are you hero?");
+
+// console.log(result);
+
+// const answer = +prompt("Вам есть 18?", "18");
+// console.log(answer + 5);
+
+// const answers = [];
+
+// answers[0] = prompt('Как ваше имя?', '');
+// answers[1] = prompt('Какая ваша фамилия?', '');
+// answers[2] = prompt('Сколько вам лет?', '');
+
+// console.log(typeof(answers));
+// console.log(typeof(null));
+
+// УРОК 8
+// const category = 'toys';
+
+// console.log(`https://sumeurl.com/${category}/5`);
+
+// const user = "Ivam";
+
+// alert(`Привет, ${user}`);
+
+
+
+
+// УРОК 9
+// console.log(4 + +" 5");
+
+// let incr = 10,
+//     decr = 10;
+
+// ++incr;
+// --decr;
+
+// console.log(++incr);
+// console.log(--decr);
+
+// console.log(5%2);
+
+// console.log(2 + 2 * 2 !== 6);
+
+// &&
+// ||
+
+// const isChecked = false,
+//       isClose = false;
+
+// console.log(isChecked || !isClose);
