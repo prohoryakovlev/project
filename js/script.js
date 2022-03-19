8de469a0f13f7f1cc85482db26d2d86b697f95e2
Prohor <prohor-yakovlev@mail.ru>
2022-03-19 13:37:01 +0300

    options

--- js/script.js
+++ js/script.js
@@ -300,7 +300,7 @@
 //   }
 // }
 
-// // rememberMyFilms();
+// rememberMyFilms();
 
 
 
@@ -315,7 +315,7 @@
 //     console.log("Произошла Ошибка!");
 //   }
 // }
-// // detectPersonalLevel();
+// detectPersonalLevel();
 
 // function showMyDB (hidden) {
 //   if (!hidden) {
@@ -360,3 +360,46 @@
 // }
 
 // learnJS('JavaScript', done);
+
+const options = {
+  name: 'test',
+  width: 1024,
+  height: 1024,
+  colors: {
+    border: 'black',
+    bg: 'red'
+  },
+  makeTest: function() {
+    console.log("Test");
+  }
+};
+
+options.makeTest();
+
+const {border, bg} = options.colors;
+console.log(border);
+
+// console.log(Object.keys(options).length);
+
+// console.log(options["colors"]["border"]);
+
+// delete options.name;
+
+// console.log(options);
+
+
+// let counter = 0;
+// for (let key in options) {
+//   if(typeof(options[key]) ==='object') {
+//     for (let i in options[key]) {
+//       console.log(`Свойство ${i} имеет значение ${options[key][i]}`);
+//       counter++;
+//     }
+//   } else {
+//     console.log(`Свойство ${key} имеет значение ${options[key]}`);
+//     counter++;
+//   }
+// }
+
+// console.log(counter);
+
