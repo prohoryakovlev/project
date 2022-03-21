0746e1ef1c116752177e1ba2a3baa65c76ab1744
Prohor <prohor-yakovlev@mail.ru>
2022-03-21 20:18:02 +0300

    prototupe

--- js/script.js
+++ js/script.js
@@ -461,43 +461,43 @@
 // console.log(copy);
 // console.log(obj);
 
-function copy (mainObj) {
-  let objCopy = {};
+// function copy (mainObj) {
+//   let objCopy = {};
 
-  let key;
-  for(key in mainObj) {
-    objCopy[key] = mainObj[key];
-  }
+//   let key;
+//   for(key in mainObj) {
+//     objCopy[key] = mainObj[key];
+//   }
 
-  return objCopy;
+//   return objCopy;
 
-}
+// }
 
-const numbers = {
-  a: 2,
-  b: 5,
-  c: {
-    x: 7,
-    y: 4
-  }
-};
+// const numbers = {
+//   a: 2,
+//   b: 5,
+//   c: {
+//     x: 7,
+//     y: 4
+//   }
+// };
 
-const newNumbers = copy(numbers);
+// const newNumbers = copy(numbers);
 
-newNumbers.a = 10;
-newNumbers.c.x = 10;
+// newNumbers.a = 10;
+// newNumbers.c.x = 10;
 
 // console.log(newNumbers);
 // console.log(numbers);
 
-const add = {
-  d: 17,
-  e: 20
-};
+// const add = {
+//   d: 17,
+//   e: 20
+// };
 
-const clone = Object.assign({}, add);
+// const clone = Object.assign({}, add);
 
-clone.d = 20;
+// clone.d = 20;
 
 // console.log(add);
 // console.log(clone);
@@ -516,25 +516,59 @@ clone.d = 20;
 
 // console.log(intenet);
 
-function log(a, b, c) {
-  console.log(a);
-  console.log(b);
-  console.log(c);
-}
+// function log(a, b, c) {
+//   console.log(a);
+//   console.log(b);
+//   console.log(c);
+// }
 
-const num = [2, 5, 7];
+// const num = [2, 5, 7];
 
-log(...num);
+// log(...num);
 
-const array = ["a", "b"];
+// const array = ["a", "b"];
 
-const newArray = [...array];
+// const newArray = [...array];
 
-const q = {
-  one: 1,
-  two: 2
+// const q = {
+//   one: 1,
+//   two: 2
+// };
+
+// const newObj = {...q};
+
+// console.log(q);
+
+
+
+let str = "some";
+
+let strObj = new String(str);
+
+// console.log(typeof(str));
+// console.log(typeof(strObj));
+
+console.dir([1, 2, 3]);
+
+const soldier = {
+  health: 400,
+  armor: 100,
+  sayHello: function() {
+    console.log("Hello!!!");
+  }
 };
 
-const newObj = {...q};
+const jonh = Object.create(soldier);
 
-console.log(q);
+// const jonh = {
+//   health: 100
+// };
+
+// jonh.__proto__ = soldier;
+
+// Object.setPrototypeOf(jonh, soldier);
+
+
+
+jonh.sayHello();
+
