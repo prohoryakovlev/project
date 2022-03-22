9317d78638017a35428cdcdfeb498f0a95a51250
Prohor <prohor-yakovlev@mail.ru>
2022-03-22 20:13:48 +0300

    typing

--- js/script.js
+++ js/script.js
@@ -649,23 +649,74 @@
 // };
 
 
-function hello() {
-  console.log("Hello world");
-  debugger;
+// function hello() {
+//   console.log("Hello world");
+//   debugger;
+// }
+// hello();
+
+// function hi() {
+//   console.log('Say hi!');
+// }
+
+// hi();
+
+// const arr = [1, 14, 4, 30, 54],
+// sorted = arr.sort(compareNum);
+
+// function compareNum(a, b) {
+//   return a - b;
+// }
+
+// console.log(sorted);
+
+// To String
+console.log(typeof(String(null)));
+console.log(typeof(String(4)));
+
+// 2
+console.log(typeof(null + ''));
+
+const num = 5;
+
+console.log("http://vk.com/catalog/" + num);
+
+const fontSize = 26 + 'px';
+
+console.log(fontSize);
+
+// To Number
+// 1
+console.log(typeof(Number('4')));
+
+// 2
+console.log(typeof(+ '5'));
+
+// 3
+console.log(typeof(parseInt("15px", 10)));
+
+
+let answ = +prompt("Hello", "");
+
+// To Boolean
+// 0, '', null, undefined, NaN;
+
+// 1
+let switcher = null;
+
+if (switcher) {
+  console.log("Working...");
 }
-hello();
 
-function hi() {
-  console.log('Say hi!');
+
+switcher = 1;
+
+if (switcher) {
+  console.log("Working...");
 }
 
-hi();
+// 2
+console.log(typeof(Boolean('4')));
 
-const arr = [1, 14, 4, 30, 54],
-sorted = arr.sort(compareNum);
-
-function compareNum(a, b) {
-  return a - b;
-}
-
-console.log(sorted);
+// 3
+console.log(typeof((!!"44444")));
